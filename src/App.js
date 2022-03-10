68d7dc47d3c735aaafc7af71a2446a125e3aa82e
Soknic <76237572+Soknic@users.noreply.github.com>
2022-03-10 13:10:37 +0100

    sdfkj

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import "./app.css";
 import { Routes, Route } from "react-router-dom";
 import AllCountries from "./component/AllCountries/AllCountries";
 import CountryInfo from "./component/CountryInfo/CountryInfo";
+import { useState } from "react";
 
 function App() {
+  const [darkModeOn, setDarkModeOn] = useState(false);
+
+  
   return (
     <>
-      <header className="header">
+      <div className={`app ${darkModeOn ? "dark" : ""}`}>
+        <header className="header">
+          <div className="container__header">
+            <h5>Where in the world</h5>
+            <p className="toggle" onClick={() => setDarkModeOn(!darkModeOn)}>
+              Toggle
+            </p>
+          </div>
+        </header>
         <div className="container">
-          <h5>Where in the world</h5>
+          <Routes>
+            <Route path="/" element={<AllCountries />} />
+            <Route path="/country/:countryName" element={<CountryInfo />} />
+          </Routes>
         </div>
-      </header>
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<AllCountries/>} />
-          <Route path="/country/:countryName" element={<CountryInfo />} />
-        </Routes>
       </div>
     </>
   );
